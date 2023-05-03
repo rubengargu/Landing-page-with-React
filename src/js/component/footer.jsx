a9a18452b99abd890cbeb3a563fd00b81d900aa9
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-05-03 19:31:49 +0000

    03/05 ok

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,9 @@
+ import React from "react";
+ const Footer =() =>{
+     return(
+         <div className="bg-secondary p-2">
+         <h2 className="color-light">Copyright & Maria and Ruben</h2>
+         </div>
+     )
+ }
+ export default Footer
