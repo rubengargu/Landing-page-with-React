b1d4233f78dc506788225ca43203fa5a7ff846f3
mariaperez15 <123765659+mariaperez15@users.noreply.github.com>
2023-05-03 19:31:34 +0000

    en proceso

---
+++ src/js/component/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+   return(
+        <div className="card">
+    <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+    </div>
+    </div>
+   )  
+}
+
+export default Header;
