9f6777284afd27b83631f6f79f07174a516e3b1e
mariaperez15 <123765659+mariaperez15@users.noreply.github.com>
2023-05-04 08:03:43 +0000

    terminado

--- src/js/component/header.js
+++ src/js/component/header.js
@@ -3,13 +3,13 @@ import React from "react";
 const Header = () => {
    return(
         <div className="card">
-    <div className="card-body">
-        <h5 className="card-title">Special title treatment</h5>
-        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
-    </div>
-    </div>
-   )  
+            <div className="card-body bg-secondary">
+                <h1 className="card-title">A Warm Welcome!</h1>
+                <p className="card-text">Lorem ipsum dolor sit amet. Non dolor iusto non veritatis deleniti in placeat dicta. In reiciendis minus et quibusdam dolores et labore aspernatur! Ad vitae esse eum atque beatae ut similique pariatur aut unde enim rem fuga quasi et numquam quia </p>
+                <a href="#" className="btn btn-primary">Call to action!</a>
+            </div>
+        </div>
+     )  
 }
 
 export default Header;
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,30 +2,30 @@ import React from "react";
 
 const Navbar = () => {
     return(
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Start Bootstrap</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarText">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Services</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark">
+          <div className="container-fluid div position-relative">
+            <a className="navbar-brand text-white" href="#">Start Bootstrap</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse position-absolute end-0 pe-5" id="navbarText">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-secondary" href="#">About</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-secondary" href="#">Services</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-secondary" href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
     )
 }
 
